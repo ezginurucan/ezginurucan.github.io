bc1fb5a1eb3caaa633335f6a66528de8c606a8ba
ezginurucan <ezginurucan55@gmail.com>
2023-05-03 16:31:01 +0300

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -1,22 +1,50 @@
 function changeTheme() {
+  var link = document.getElementsByTagName("link")[4];
+  if (link.getAttribute("href") == "css/styles.css") {
+    link.setAttribute("href", "css/light.css");
+    document.querySelector(".theme-button").innerHTML = "Dark Theme";
+    // Set cookie to remember theme preference
+    document.cookie = "theme=light";
+  } else {
+    link.setAttribute("href", "css/styles.css");
+    document.querySelector(".theme-button").innerHTML = "Light Theme";
+    // Set cookie to remember theme preference
+    document.cookie = "theme=dark";
+  }
+}
+
+function mousein(){
+  var element = document.getElementById("buton");
+  element.style.backgroundColor= "#214e77";
+  element.style.border="aliceblue";
+  element.style.color="aliceblue";
+}
+
+function mouseOut(){
+  var element = document.getElementById("buton");
+  element.style.backgroundColor= "";
+}
+
+// Check for saved theme preference on page load
+window.onload = function() {
+  var savedTheme = getCookie("theme");
+  if (savedTheme) {
     var link = document.getElementsByTagName("link")[4];
-    if (link.getAttribute("href") == "css/styles.css") {
+    if (savedTheme === "light") {
       link.setAttribute("href", "css/light.css");
       document.querySelector(".theme-button").innerHTML = "Dark Theme";
-     
     } else {
       link.setAttribute("href", "css/styles.css");
       document.querySelector(".theme-button").innerHTML = "Light Theme";
     }
+  }
 }
 
-function mousein(){
-    var element = document.getElementById("buton");
-    element.style.backgroundColor= "#214e77";
-    element.style.border="aliceblue";
-    element.style.color="aliceblue";
-}
-function mouseOut(){
-  var element = document.getElementById("buton");
-  element.style.backgroundColor= "";
+// Helper function to retrieve cookie value
+function getCookie(name) {
+  var value = "; " + document.cookie;
+  var parts = value.split("; " + name + "=");
+  if (parts.length == 2) {
+    return parts.pop().split(";").shift();
+  }
 }
